98737ca780dec275bfd7225ad325fc3feb2303cf
tanh1c <chunguyentuananh11b6@gmail.com>
2025-09-14 21:35:02 +0700

    Update 2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Use esbuild instead of terser
     rollupOptions: {
       output: {
         manualChunks: {
